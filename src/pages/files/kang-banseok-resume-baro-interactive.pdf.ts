46fd830413e2b6241133375cd7011862efc775f7
BANSEOK-KANG1 <kangbs2486@gmail.com>
2026-07-21 08:03:06 +0900

    Use corrected PDF chunk split for Baro resume

--- src/pages/files/kang-banseok-resume-baro-interactive.pdf.ts
+++ src/pages/files/kang-banseok-resume-baro-interactive.pdf.ts
@@ -3,13 +3,14 @@ import part2 from '../../data/baroResumePdfChunks/part2';
 import part3 from '../../data/baroResumePdfChunks/part3';
 import part4 from '../../data/baroResumePdfChunks/part4';
 import part5 from '../../data/baroResumePdfChunks/part5';
-import part6 from '../../data/baroResumePdfChunks/part6';
+import part6a from '../../data/baroResumePdfChunks/part6a';
+import part6b from '../../data/baroResumePdfChunks/part6b';
 import part7 from '../../data/baroResumePdfChunks/part7';
 
 export const prerender = true;
 
 export function GET() {
-  const base64 = [part1, part2, part3, part4, part5, part6, part7].join('');
+  const base64 = [part1, part2, part3, part4, part5, part6a, part6b, part7].join('');
   const binary = globalThis.atob(base64);
   const bytes = new Uint8Array(binary.length);
 
